7cbb1a5d6abda2d918c0483dbabafedc444bccad
LT-Nhat-Minh <22520863@gm.uit.edu.vn>
2024-10-13 21:07:38 +0700

    minh test conflict

--- src/pages/Volunteer/index.jsx
+++ src/pages/Volunteer/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Volunteer(props) {
-  return <div>Tình Nguyện Viên</div>;
+  return <div>Tình Nguyện Viên Nhat minh test conflict</div>;
 }
 
 export default Volunteer;
