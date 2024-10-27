18c8e491de352ea60ce66db1c7c1eae2082a56aa
LT-Nhat-Minh <22520863@gm.uit.edu.vn>
2024-10-27 16:26:09 +0700

    minh update contact info

--- src/pages/Contact/ListSponsor/index.jsx
+++ src/pages/Contact/ListSponsor/index.jsx
@@ -1,13 +1,31 @@
-import React from "react";
-import "./style.scss"
+import React, { useState } from "react";
+import "./style.scss";
+import { Modal } from "antd";
 
 function Listsponsor(props) {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <div class="banner4">
-        <div class="overlay4">
-            <h1>Danh sách mạnh thường quân</h1>
-            <button>XEM THÔNG TIN</button>
-        </div>
+      <div class="overlay4">
+        <h1>Danh sách mạnh thường quân</h1>
+        <button
+          onClick={() => {
+            setIsModalOpen(true);
+          }}
+        >
+          XEM THÔNG TIN
+        </button>
+        <Modal title="Basic Modal" open={isModalOpen} onCancel={handleCancel}>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </div>
     </div>
   );
 }
